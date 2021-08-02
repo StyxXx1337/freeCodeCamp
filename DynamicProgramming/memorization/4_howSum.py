8034ddbe59fcd5c8af75e7ac3581d6d4d37d28d9
Styx1337 <kefin87@gmail.com>
2021-08-02 11:43:52 +0900

    Updated Problem 4:  howSum

--- DynamicProgramming/memorization/4_howSum.py
+++ DynamicProgramming/memorization/4_howSum.py
@@ -26,9 +26,9 @@ def howSum_memo(target_sum: int, numbers: list[int], memo=None) -> list[int]:
 
     for num in numbers:
         remainder = target_sum - num
-        if howSum_memo(remainder, numbers, memo) is not None:
-            result = howSum_memo(remainder, numbers, memo)
-            memo[remainder] = result.copy()
+        result = howSum_memo(remainder, numbers, memo)
+        if result is not None:
+            memo[remainder] = result
             result.append(num)
             return result
 
@@ -37,15 +37,17 @@ def howSum_memo(target_sum: int, numbers: list[int], memo=None) -> list[int]:
 
 
 print("======================== How Sum without memorization ==============================")
-print(howSum(7, [5, 3, 4, 7]))  # [4, 3]
-print(howSum(7, [2, 4]))  # False
-print(howSum(8, [5, 2, 4, 6]))  # [2, 2, 2, 2]
-print(howSum(17, [11, 4, 7]))  # False
-# print(howSum(300, [7, 14]))        # False --> Takes very long
+print(howSum(7, [5, 3, 4, 7]))   # [4, 3]
+print(howSum(7, [2, 4]))         # None
+print(howSum(8, [5, 2, 4, 6]))   # [2, 2, 2, 2]
+print(howSum(17, [11, 4, 7]))    # None
+# print(howSum(300, [7, 14]))      # False --> Takes very long
+# print(howSum(300, [1, 14]))      # [1, ..., 1] --> Takes very long
 print()
 print("======================== How Sum with memorization ==============================")
 print(howSum_memo(7, [5, 3, 4, 7]))  # [4, 3]
-print(howSum_memo(7, [2, 4]))  # False
+print(howSum_memo(7, [2, 4]))        # None
 print(howSum_memo(8, [5, 2, 4, 6]))  # [2, 2, 2, 2]
-print(howSum_memo(17, [11, 4, 7]))  # False
+print(howSum_memo(17, [11, 4, 7]))   # None
 print(howSum_memo(300, [7, 14]))  # False
+print(len(howSum_memo(300, [1, 14])))  # 300
